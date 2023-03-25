0c03bb8743843a1f5776802f8dc69413c9fb7e7c
Barbara Lin <linbarbaralulu@gmail.com>
2023-03-24 22:51:22 -0400

    added shirts, attempted to remove semicolon

--- frontend/src/Products/data.tsx
+++ frontend/src/Products/data.tsx
@@ -16,11 +16,27 @@ export default {
         numberOfReviews: 10,
     },
     {
+        name: 'Jersey',
+        imageUrl: '/images/d1.jpg',
+        price: 100,
+        brand: 'Adidas',
+        rating: 5.0,
+        numberOfReviews: 20,
+    },
+    {
         name: 'Shirt',
         imageUrl: '/images/d1.jpg',
-        price: 60,
+        price: 20,
         brand: 'Nike',
-        rating: 4.5,
-        numberOfReviews: 10,
+        rating: 3.5,
+        numberOfReviews: 5,
+    }, 
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 20,
+        brand: 'Nike',
+        rating: 3.5,
+        numberOfReviews: 5
     }]
 }
